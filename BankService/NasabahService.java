806dbc9b19ad01ec167db51793bf0a863917c4dc
Willsen <willsen_s@yahoo.co.id>
2019-11-23 12:50:33 +0700

    add validasi no rek functionality

---
+++ BankService/NasabahService.java
@@ -0,0 +1,30 @@
+package BankService;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.jws.WebService;
+
+import BankDomain.Nasabah;
+
+@WebService
+public class NasabahService {
+	
+	public boolean isNoRekValid(String no) {
+		try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/example", "postgres", "postgres")) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM public.customer");
+            while (resultSet.next()) {
+                if(no == resultSet.getString("no_rek")) {
+                	return true;
+                }
+            }
+            return false;
+        }catch (SQLException e) {
+            	System.out.println("Connection failure.");
+            	e.printStackTrace();
+        }
+		return false;
+	}
+}
