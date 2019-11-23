08409867e7f2fe15ce7cb7cc23d8f4e220ecf2e7
Willsen <willsen_s@yahoo.co.id>
2019-11-23 14:21:14 +0700

    add virtual acc func

--- BankService/NasabahService.java
+++ BankService/NasabahService.java
@@ -10,5 +10,15 @@ import BankDomain.Nasabah;
 
 @WebService
 public class NasabahService {
-
+    public void addVirtualAcc(int no) {
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/example", "postgres", "postgres")) {
+            Statement statement = connection.createStatement();
+            String generatedString = RandomStringUtils.random(9, true, true);
+            statement.executeUpdate("INSERT INTO virtual VALUES (" + no +", '" + generatedString +"');");
+        }
+        catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
+    }
 }
