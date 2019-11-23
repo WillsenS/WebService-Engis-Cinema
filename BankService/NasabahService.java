f6c209b37e04ddf166cdb587df8ee5af1001cb24
Willsen <willsen_s@yahoo.co.id>
2019-11-23 12:56:52 +0700

    add Nasabah Service

---
+++ BankService/NasabahService.java
@@ -0,0 +1,14 @@
+package BankService;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.jws.WebService;
+
+import BankDomain.Nasabah;
+
+@WebService
+public class NasabahService {
+
+}
