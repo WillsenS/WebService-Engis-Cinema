d868b4b5dee7355bcf2346e48ba9e6d357e2203c
Willsen <willsen_s@yahoo.co.id>
2019-11-24 13:56:57 +0700

    fixed bug with database connecting, add import apache.lang

--- BankService/NasabahService.java
+++ BankService/NasabahService.java
@@ -4,14 +4,14 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import org.apache.commons.lang3.RandomStringUtils;
 import javax.jws.WebService;
-
 import BankDomain.Nasabah;
 
 @WebService
 public class NasabahService {
     public void addVirtualAcc(int no) {
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/example", "postgres", "postgres")) {
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost/WebServiceBank", "postgres", "")) {
             Statement statement = connection.createStatement();
             String generatedString = RandomStringUtils.random(9, true, true);
             statement.executeUpdate("INSERT INTO virtual VALUES (" + no +", '" + generatedString +"');");
